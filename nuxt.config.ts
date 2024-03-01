7cf73739e9007b9272145bd79a5dd27da37256e9
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-01 21:18:29 +0100

    feat: add linter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/image", "@vueuse/nuxt", "@nuxtjs/tailwindcss"],
-});
+  css: ['~/assets/css/main.css'],
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+  ],
+})
