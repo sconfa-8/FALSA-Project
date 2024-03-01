f93b96be21b17e9760271a19f9c5b3eeb2874d73
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-02 00:32:55 +0100

    feat: chatbot cmp

---
+++ components/Chatbot.vue
@@ -0,0 +1,9 @@
+<template>
+  <iframe
+    src="https://www.chatbase.co/chatbot-iframe/-H1nY5RpRk00Qg9XEoBis"
+    title="RetexWelcome"
+    width="100%"
+    style="height: 100%; min-height: 700px"
+    frameborder="0"
+  ></iframe>
+</template>
