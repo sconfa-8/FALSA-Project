3e9543e6a001f344f75da5735816d9822768d8d1
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-01 23:00:42 +0100

    feat: init layout

--- app.vue
+++ app.vue
@@ -1,21 +1,18 @@
 <script setup>
-const email = ref("MarioRossi");
-const password = ref("Ciao123!");
+const email = ref('MarioRossi')
+const password = ref('Ciao123!')
 
-const { login, isLogged } = useAuth();
+const { login, isLogged } = useAuth()
 
 const submitLogin = () => {
-  login(email.value, password.value);
-};
+  login(email.value, password.value)
+}
 </script>
 
 <template>
   <div>
-    <form class="flex flex-col gap-2 bg-slate-500" action="">
-      <input v-model="email" type="text" />
-      <input type="password" v-model="password" />
-      <button type="button" @click="submitLogin">Login</button>
-    </form>
-    {{ isLogged }}
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
