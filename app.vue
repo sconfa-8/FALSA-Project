0339d1de894b56198830b67159d7fca633aaf8e0
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-01 22:46:58 +0100

    chore: ref prefill

--- app.vue
+++ app.vue
@@ -1,19 +1,19 @@
 <script setup>
-const email = ref()
-const password = ref()
+const email = ref("MarioRossi");
+const password = ref("Ciao123!");
 
-const {login,isLogged} = useAuth()
+const { login, isLogged } = useAuth();
 
 const submitLogin = () => {
-  const isLogged = login(email.value,password.value)
-}
+  login(email.value, password.value);
+};
 </script>
 
 <template>
   <div>
     <form class="flex flex-col gap-2 bg-slate-500" action="">
-      <input  v-model="email" type="text">
-      <input v-model="password" type="text">
+      <input v-model="email" type="text" />
+      <input type="password" v-model="password" />
       <button type="button" @click="submitLogin">Login</button>
     </form>
     {{ isLogged }}
