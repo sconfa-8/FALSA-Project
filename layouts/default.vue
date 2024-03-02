c98e00d639f6cd5e4a812834dea8009b8e9e5578
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-02 10:42:34 +0100

    feat: hife feedback for hr

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,7 +24,7 @@ const isHR = fullPath.split('/').at(1) === 'hr'
     <div
     class="fixed bottom-0 right-0 left-0 bg-white flex-shrink-0 flex-grow-0"
     >
-    <OrganismsFeedbackUser />
+    <OrganismsFeedbackUser v-if="!isHR" />
     <OrganismsMenuHr v-if="isHR" />
     <OrganismsMenuUser v-else />
   </div>
