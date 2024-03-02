647cc3d75bb73f9cb77d65ed657883678250ce8c
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-02 10:30:14 +0100

    aaaa

--- pages/user/news.vue
+++ pages/user/news.vue
@@ -13,11 +13,8 @@ import usersData from '../data/events.json'
     <div
       v-for="(item, index) in usersData"
       :key="index"
-      class="grid grid-cols-12 gap-2 rounded-lg border border-black"
+      class="grid grid-cols-12 gap-2 rounded-lg border border-black px-4 md:px-8"
     >
-      <div class="w-full h-full col-span-3 flex justify-center items-center">
-        <p>img</p>
-      </div>
       <div class="col-span-9 pr-2 py-4">
         <h3 class="font-bold">{{ item.title }}</h3>
         <p>
