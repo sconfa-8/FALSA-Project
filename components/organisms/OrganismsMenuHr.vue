26c398395ee51d6d89f5cafdb0eb3cab2864cec5
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-02 03:51:06 +0100

    feat: style for chips

--- components/organisms/OrganismsMenuHr.vue
+++ components/organisms/OrganismsMenuHr.vue
@@ -25,7 +25,7 @@ const menuItems = [
       v-for="(item, index) in menuItems"
       :key="index"
       :to="item.url"
-      class="flex flex-col items-center text-center"
+      class="flex flex-col items-center text-center bg-slate-300 m-2 hover:bg-slate-400 transition-colors ease-in duration-300 rounded-lg"
     >
       <p>▢</p>
       <p>{{ item.label }}</p>
