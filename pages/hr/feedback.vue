063446886a04c449c9bcd767dd524927e1f306eb
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-02 05:25:55 +0100

    feat: init hr notification

--- pages/hr/feedback.vue
+++ pages/hr/feedback.vue
@@ -1,7 +1,56 @@
 <script setup></script>
 
 <template>
-  <div>
-    <p>This is Feedback page</p>
+  <div class="space-y-4">
+    <p class="underline">Feedback</p>
+
+    <div class="flex gap-8">
+      <div class="h-4 w-4 mt-4 rounded-full border-2 border-black"></div>
+      <div class="space-y-2">
+        <h3>Pino Pasticcio @ RetexWelcome</h3>
+        <div class="flex gap-2">
+          <IconsStar class="w-4 h-4 text-gray-305000" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+        </div>
+        <button class="bg-gray-300 rounded-md text-white px-6 text-sm">
+          Rispondi
+        </button>
+      </div>
+    </div>
+    <div class="flex gap-8">
+      <div class="h-4 w-4 mt-4 rounded-full border-2 border-black"></div>
+      <div class="space-y-2">
+        <h3>Pino Pasticcio @ RetexWelcome</h3>
+        <div class="flex gap-2">
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-transparent" />
+          <IconsStar class="w-4 h-4 text-transparent" />
+        </div>
+        <button class="bg-gray-300 rounded-md text-white px-6 text-sm">
+          Rispondi
+        </button>
+      </div>
+    </div>
+    <div class="flex gap-8">
+      <div class="h-4 w-4 mt-4 rounded-full border-2 border-black"></div>
+      <div class="space-y-2">
+        <h3>Pino Pasticcio @ RetexWelcome</h3>
+        <div class="flex gap-2">
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-gray-500" />
+          <IconsStar class="w-4 h-4 text-transparent" />
+        </div>
+        <button class="bg-gray-300 rounded-md text-white px-6 text-sm">
+          Rispondi
+        </button>
+      </div>
+    </div>
   </div>
 </template>
