0aff243f7c2586bd5282ae5826eb5c22285f069c
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-02 05:38:42 +0100

    feat: notification page

--- pages/hr/notification.vue
+++ pages/hr/notification.vue
@@ -1,7 +1,64 @@
 <script setup></script>
 
 <template>
-  <div>
-    <p>This is Notification page</p>
+  <div class="space-y-6">
+    <p class="underline text-xl">Notifiche</p>
+
+    <p class="uppercase bg-gray-500 text-white w-full px-2">Da leggere</p>
+
+    <!-- Cards -->
+    <div class="space-y-2">
+      <p>@Pino Pasticcio</p>
+      <div class="border border-gray-400 rounded-2xl p-4">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="w-full flex justify-end px-4">
+        <button class="text-gray-500 underline">Letta</button>
+      </div>
+    </div>
+
+    <div class="space-y-2">
+      <p>@Pino Pasticcio</p>
+      <div class="border border-gray-400 rounded-2xl p-4">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="w-full flex justify-end px-4">
+        <button class="text-gray-500 underline">Letta</button>
+      </div>
+    </div>
+
+    <p class="uppercase bg-gray-500 text-white w-full px-2">Lette</p>
+
+    <div class="space-y-2">
+      <p>@Pino Pasticcio</p>
+      <div class="border border-gray-400 rounded-2xl p-4">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="w-full flex justify-end px-4">
+        <button class="text-gray-500 underline">Elimina</button>
+      </div>
+    </div>
+
+    <div class="space-y-2">
+      <p>@Pino Pasticcio</p>
+      <div class="border border-gray-400 rounded-2xl p-4">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="w-full flex justify-end px-4">
+        <button class="text-gray-500 underline">Elimina</button>
+      </div>
+    </div>
   </div>
 </template>
