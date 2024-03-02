10d01c5074df3eb219e635ef562788e4231c0bc8
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-02 09:44:11 +0100

    feat: user icon

--- components/organisms/OrganismsMenuUser.vue
+++ components/organisms/OrganismsMenuUser.vue
@@ -25,15 +25,22 @@ const menuItems = [
 </script>
 
 <template>
-  <div class="grid grid-cols-4 justify-between w-full border-t-black border-t">
-    <NuxtLink
-      v-for="(item, index) in menuItems"
-      :key="index"
-      :to="item.url"
-      class="flex flex-col items-center text-center"
-    >
-      <p>▢</p>
-      <p>{{ item.label }}</p>
+  <div class="grid grid-cols-4 justify-between w-full border-t-black border-t pt-2">
+    <NuxtLink to="/user" class="flex flex-col items-center text-center">
+      <IconsCalendar class="h-4 w-4"/>
+      <p>Home</p>
+    </NuxtLink>
+    <NuxtLink to="/user/calendar" class="flex flex-col items-center text-center">
+      <IconsCalendar class="h-4 w-4" />
+      <p>Calendario</p>
+    </NuxtLink>
+    <NuxtLink to="/user/info" class="flex flex-col items-center text-center">
+      <IconsInfo class="h-4 w-4" />
+      <p>Info Utili</p>
+    </NuxtLink>
+    <NuxtLink to="/user/news" class="flex flex-col items-center text-center">
+      <IconsNews class="h-4 w-4"/>
+      <p>News</p>
     </NuxtLink>
   </div>
 </template>
