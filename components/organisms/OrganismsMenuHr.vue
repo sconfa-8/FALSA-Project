e9f75d4b6d20fcba059c70788d15ff8499d7b8c5
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-02 09:57:19 +0100

    feat: bg icons & hr menu black

--- components/organisms/OrganismsMenuHr.vue
+++ components/organisms/OrganismsMenuHr.vue
@@ -1,19 +1,29 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div class="grid grid-cols-3 justify-between w-full border-t-black border-t pt-2">
-    <NuxtLink to="/hr" class="flex flex-col items-center text-center">
+  <div
+    class="grid grid-cols-3 justify-between w-full bg-gray-800 border-t-black border-t py-4"
+  >
+    <NuxtLink
+      to="/hr"
+      class="flex flex-col items-center justify-between text-center"
+    >
       <IconsHome class="h-4 w-4" />
-      <p>Home</p>
+      <p class="text-white text-sm">Home</p>
     </NuxtLink>
-    <NuxtLink to="/hr/feedback" class="flex flex-col items-center text-center">
-      <IconsFeedback class="h-4 w-4"/>
-      <p>Feedback</p>
+    <NuxtLink
+      to="/hr/feedback"
+      class="flex flex-col justify-between items-center text-center"
+    >
+      <IconsFeedback class="h-4 w-4" />
+      <p class="text-white text-sm">Feedback</p>
     </NuxtLink>
-    <NuxtLink to="/hr/notification" class="flex flex-col items-center text-center">
-      <IconsNotifications class="h-4 w-4"/>
-      <p>Notification</p>
+    <NuxtLink
+      to="/hr/notification"
+      class="flex flex-col justify-between items-center text-center"
+    >
+      <IconsNotifications class="h-4 w-4" />
+      <p class="text-white text-sm">Notification</p>
     </NuxtLink>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,10 +4,15 @@ const isHR = fullPath.split('/').at(1) === 'hr'
 </script>
 
 <template>
-  <div class="max-h-[100vh] overflow-auto">
-    <div
-      class="fixed top-0 right-0 left-0 bg-white flex items-center justify-around p-4 flex-shrink-0 flex-grow-0"
-    >
+  <div class="relative">
+
+    <div class="h-screen fixed w-screen bg-gradient-light">
+      
+    </div>
+    <div class="max-h-[100vh] overflow-auto">
+      <div
+      class="fixed top-0 right-0 left-0 bg-white flex items-center justify-between p-4 flex-shrink-0 flex-grow-0"
+      >
       <p class="text-2xl">RetexWelcome</p>
       <NuxtLink to="/">
         <Logo class="h-10 w-10" />
@@ -17,11 +22,22 @@ const isHR = fullPath.split('/').at(1) === 'hr'
       <slot />
     </div>
     <div
-      class="fixed bottom-0 right-0 left-0 bg-white flex-shrink-0 flex-grow-0"
+    class="fixed bottom-0 right-0 left-0 bg-white flex-shrink-0 flex-grow-0"
     >
-      <OrganismsFeedbackUser />
-      <OrganismsMenuHr v-if="isHR" />
-      <OrganismsMenuUser v-else />
-    </div>
+    <OrganismsFeedbackUser />
+    <OrganismsMenuHr v-if="isHR" />
+    <OrganismsMenuUser v-else />
   </div>
+</div>
+</div>
 </template>
+
+<style scooped>
+.bg-gradient-light {
+  background-image: url('/retex-gradient-light.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  z-index: -10;
+}
+</style>
