e88640346883e4d1879c28ccc2b7b6399ca8c0a5
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-02 09:11:25 +0100

    feat: login style & font family

--- pages/index.vue
+++ pages/index.vue
@@ -22,16 +22,47 @@ const submitLogin = () => {
 </script>
 
 <template>
-  <div>
-    <form class="flex flex-col gap-2 bg-slate-500">
-      <input v-model="email" type="text" />
-      <input type="password" v-model="password" />
-      <button type="button" @click="submitLogin">Login</button>
-    </form>
-    <Modal
-      ><template #trigger><button>Test</button></template
-      ><template #title>Title</template
-      ><template #description>Description</template></Modal
-    >
+  <div
+    class="w-full bg-gradient m-auto h-screen flex justify-center items-center"
+  >
+    <div class="space-y-20 w-4/5 max-w-[400px]">
+      <div>
+        <LogoDark class="h-28 w-28 m-auto" />
+      </div>
+
+      <form class="flex flex-col w-full gap-4">
+        <input
+          v-model="email"
+          type="text"
+          class="border-2 border-black rounded-lg p-4"
+        />
+        <input
+          type="password"
+          v-model="password"
+          class="border-2 border-black rounded-lg p-4"
+        />
+        <button
+          type="button"
+          @click="submitLogin"
+          class="bg-black text-white rounded-lg p-4"
+        >
+          Accedi
+        </button>
+      </form>
+      <Modal
+        ><template #trigger><button>Test</button></template
+        ><template #title>Title</template
+        ><template #description>Description</template></Modal
+      >
+    </div>
   </div>
 </template>
+
+<style scooped>
+.bg-gradient {
+  background-image: url('/retex-gradient.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Poppins'],
+    },
     extend: {
-      colors:{
-        'retex':'#8fCECA'
-      }
+      colors: {
+        retex: '#8fCECA',
+      },
     },
   },
   plugins: [],
-};
+}
