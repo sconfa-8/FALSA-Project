3e231b4da90671e9fc7d7ca329e728eec19fe668
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-02 06:01:24 +0100

    feat: form new

---
+++ components/ModalForm.vue
@@ -0,0 +1,83 @@
+<script setup>
+const isOpen = useState('isOpen', () => false)
+</script>
+
+<template>
+  <Transition name="fade">
+    <div
+      class="fixed overflow-y-scroll  inset-0 md:inset-20 z-[999] bg-white rounded-lg shadow-lg flex flex-col gap-4 p-8"
+      v-if="isOpen"
+    >
+      <div class="form flex items-end justify-between pr-4 py-4">
+        <p>Inserisci nuovo impiegato</p>
+        <button class="items-end" @click="isOpen = false">
+          <IconsClose class="h-8 w-8" />
+        </button>
+      </div>
+      <div class="w-full flex flex-col md:flex-row gap-4">
+        <div class="w-full md:w-1/2">
+          Nome:
+          <Input>Mario</Input>
+        </div>
+        <div class="w-full md:w-1/2">
+          Cognome:
+          <Input>Rossi</Input>
+        </div>
+      </div>
+      <div class="w-full flex flex-col md:flex-row gap-4">
+        <div class="w-full md:w-1/2">
+          Ruolo:
+          <Select>Developer</Select>
+        </div>
+        <div class="w-full md:w-1/2">
+          Divisione:
+          <Select>Atoms</Select>
+        </div>
+      </div>
+      <div class="w-full flex flex-col md:flex-row gap-4">
+        <div class="w-full md:w-1/2">
+          Sede:
+          <Select>Mestrino</Select>
+        </div>
+        <div class="w-full md:w-1/2">
+          Email:
+          <Select>mario.rossi@atoms.retexspa.com</Select>
+        </div>
+      </div>
+      <div>
+        Data di ingresso:
+          <div class="w-full flex gap-4 items-center">
+          <Input class="w-1/3">13</Input>
+          <p>/</p>
+          <Input class="w-1/3">02</Input>
+          <p>/</p>
+          <Input class="w-1/3">2024</Input>
+        </div>
+      </div>
+      <div class="h-[1px] bg-slate-500 my-2"></div>
+      <div class="w-full flex flex-col md:flex-row gap-4">
+        <div class="w-full md:w-1/2">
+          Team Leader:
+          <Select>Massimo Pegoraro</Select>
+        </div>
+        <div class="w-full md:w-1/2">
+          Buddy:
+          <Select>Simone Confalonieri</Select>
+        </div>
+      </div>
+    </div>
+    
+  </Transition>
+</template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- components/UsersCardsList.vue
+++ components/UsersCardsList.vue
@@ -1,3 +1,7 @@
+<script setup>
+const isOpen = useState('isOpen')
+</script>
+
 <template>
   <p class="text-xl underline pb-4">Calendario incontri</p>
   <div class="w-full flex pb-2">
@@ -5,20 +9,29 @@
     <p class="font-bold w-1/2 flex">Data Ingresso</p>
   </div>
   <div class="user-cards flex flex-col gap-2">
-    <div class="w-full flex relative">
-      <p class="w-1/2">Pino Pasticcio</p>
-      <p class="w-1/2">12-02-2023</p>
-      <IconsArrowLeft class="h-4 w-4 absolute right-0" />
+    <div>
+      <div class="w-full flex relative" @click="isOpen = true">
+        <p class="w-1/2">Pino Pasticcio</p>
+        <p class="w-1/2">12-02-2023</p>
+        <IconsArrowLeft class="h-4 w-4 absolute right-0" />
+      </div>
+
+      <div class="h-[1px]  bg-slate-700 my-2" />
     </div>
-    <div class="w-full flex relative">
-      <p class="w-1/2">Mario Rossi</p>
-      <p class="w-1/2">26-01-2023</p>
-      <IconsArrowLeft class="h-4 w-4 absolute right-0" />
+    <div>
+      <div class="w-full flex relative" @click="isOpen = true">
+        <p class="w-1/2">Mario Rossi</p>
+        <p class="w-1/2">26-01-2023</p>
+        <IconsArrowLeft class="h-4 w-4 absolute right-0" />
+      </div>
+
+      <div class="h-[1px]  bg-slate-700 my-2" />
     </div>
-    <div class="w-full flex relative">
+    <div class="w-full flex relative" @click="isOpen = true">
       <p class="w-1/2">Luigi Verdi</p>
       <p class="w-1/2">13-01-2023</p>
       <IconsArrowLeft class="h-4 w-4 absolute right-0" />
     </div>
+    <ModalForm />
   </div>
 </template>
