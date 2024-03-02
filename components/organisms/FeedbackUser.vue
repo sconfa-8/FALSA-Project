64c809cf638467f3033195bae0e12e81b2c8a90b
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-02 02:21:30 +0100

    feat: feedback

---
+++ components/organisms/FeedbackUser.vue
@@ -0,0 +1,34 @@
+<script setup>
+const isOpen = ref(false)
+const isConfirm = ref(false)
+
+const clickHandler = () => {
+    isOpen.value = false
+    isConfirm.value = true
+    isDefinitive.value = true
+    setTimeout(() => {
+        isConfirm.value = false
+    }, 5000);
+}
+const isDefinitive = ref(false)
+</script>
+<template>
+  <div class="w-full">
+    <div v-if="!isConfirm && !isDefinitive" @click="isOpen = true" class="cursor-pointer">
+      <p @click="isOpen = true" class="bg-slate-400 p-2">
+        Ci dai un feedback sul tuo primo giorno?
+      </p>
+    </div>
+
+    <div>
+      <Transition>
+        <div v-if="isOpen" class="flex gap-2">
+          <IconsStar class="h-4 w-4 cursor-pointer" v-for="i in 5" @click="clickHandler"/>
+        </div>
+      </Transition>
+    </div>
+    <div v-if="isConfirm">
+        <p class="text-green-500">Grazie della risposta!</p>
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,24 @@
 <script setup>
 const { fullPath } = useRoute()
+const isHR = fullPath.split('/').at(1) === 'hr'
 </script>
 
 <template>
   <div class="max-h-[100vh]">
     <div class="flex items-center justify-around p-4 flex-shrink-0 flex-grow-0">
-      <p class="text-2xl ">
+      <p class="text-2xl">
         RetexWelcome
       </p>
       <NuxtLink to="/">
         <Logo class="h-10 w-10"/>
       </NuxtLink>
     </div>
-    <div class="h-[calc(100vh_-_120px)]">
+    <div class="h-[calc(100vh_-_160px)]">
       <slot />
     </div>
-
-    <div class="flex-shrink-0 flex-grow-0">
-      <OrganismsMenuHr v-if="fullPath.split('/').at(1) === 'hr'" />
+    <OrganismsFeedbackUser />
+    <div class="relative flex-shrink-0 flex-grow-0">
+      <OrganismsMenuHr v-if="isHR" />
       <OrganismsMenuUser v-else />
     </div>
   </div>
