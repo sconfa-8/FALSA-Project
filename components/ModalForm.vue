cafb1b42ce37e9ec05626c28a2de3dd364778503
luca.costa <100123915+luca-costa@users.noreply.github.com>
2024-03-02 06:10:39 +0100

    fix: font

--- components/ModalForm.vue
+++ components/ModalForm.vue
@@ -8,7 +8,7 @@ const isOpen = useState('isOpen', () => false)
       class="fixed overflow-y-scroll  inset-0 md:inset-20 z-[999] bg-white rounded-lg shadow-lg flex flex-col gap-4 p-8"
       v-if="isOpen"
     >
-      <div class="form flex items-end justify-between pr-4 py-4">
+      <div class="form flex items-end justify-between pr-4 py-4 text-2xl">
         <p>Inserisci nuovo impiegato</p>
         <button class="items-end" @click="isOpen = false">
           <IconsClose class="h-8 w-8" />
