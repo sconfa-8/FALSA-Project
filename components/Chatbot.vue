7f16ea55c8e21cd98c36eaadd1f74139736d434e
Simone Confalonieri <82416028+SimoneConfalonieri8@users.noreply.github.com>
2024-03-18 16:50:51 +0100

    feat(main): change chatbot iframe

--- components/Chatbot.vue
+++ components/Chatbot.vue
@@ -1,9 +1,11 @@
 <template>
-  <iframe
-    src="https://www.chatbase.co/chatbot-iframe/-H1nY5RpRk00Qg9XEoBis"
+  <div class="">
+    <iframe
+    src="https://www.chatbase.co/chatbot-iframe/I_Q-1vO77SCxTGW6zncE-"
     title="RetexWelcome"
     width="100%"
-    style="height: 100%; min-height: 700px"
+    style="height: 80vh; min-height: 100%"
     frameborder="0"
-  ></iframe>
+    ></iframe>
+  </div>
 </template>
